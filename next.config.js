43f129fc9693706646b8202ac426d782121491e9
Jim Ezesinachi <ezesinachijim@gmail.com>
2025-07-04 20:01:48 +0100

    chore: remove logger

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const isCloudflareWorker = process.env.DEPLOY_TARGET === "cloudflare";
 
-console.log(
-  `🛠️ Building for: ${process.env.DEPLOY_TARGET || "default"} → ${isCloudflareWorker ? "static export" : "SSR"}`,
-);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: isCloudflareWorker ? "export" : undefined,
